b94c0b1ee749a58e5c35d49a68175db7dc207f49
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-10-22 16:49:15 +0300

    Update Enlarge.jsx

--- src/Enlarge.jsx
+++ src/Enlarge.jsx
@@ -6,39 +6,6 @@ import cx from 'classnames';
 const Enlarge = ({ children, className, onClick, ref, block }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    if (isOpen === true) {
-      let svg2 = document.getElementById(
-        'embed_svg_modal' + block,
-      )?.firstElementChild;
-
-      if (svg2) {
-        let width = svg2.getAttribute('width');
-        let height = svg2.getAttribute('height');
-
-        if (!width || !height) {
-          const viewBox = svg2.getAttribute('viewBox');
-          if (viewBox) {
-            const viewBoxValues = viewBox.split(' ');
-            if (viewBoxValues.length === 4) {
-              width = viewBoxValues[2]; // width from viewBox
-              height = viewBoxValues[3]; // height from viewBox
-            }
-          }
-        }
-
-        if (width && height) {
-          svg2.setAttribute(
-            'viewBox',
-            `0 0 ${parseFloat(width)} ${parseFloat(height)}`,
-          );
-          svg2.setAttribute('height', '100%');
-          svg2.setAttribute('width', '100%');
-        }
-      }
-    }
-  }, [block, isOpen]);
-
   return (
     <div className="enlarge">
       <button
