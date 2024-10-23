60a684d7ed2e0befce35572c199e71144cbd76df
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-10-23 14:34:18 +0300

    show safe image safe just on svgs

--- src/Embed.jsx
+++ src/Embed.jsx
@@ -15,14 +15,51 @@ import Download from './DownloadData';
 import Enlarge from './Enlarge';
 import { getFileExtension } from './helpers';
 
-function Embed({ data, screen, block }) {
+function Embed(props) {
+  const { data, screen, block } = props;
   const el = useRef();
   const modal = useRef();
   const [svg, setSVG] = useState('');
   const [mobile, setMobile] = useState(false);
-  console.log(data);
-  const isSvg = getFileExtension(data.preview_image) === 'svg';
 
+  const isSvg = getFileExtension(data.preview_image) === 'svg';
+  if (
+    !isSvg &&
+    props?.modifiedSchema?.fieldsets?.[0]?.fields?.includes('safe_load_img')
+  ) {
+    props.setModifiedSchema({
+      ...props.modifiedSchema,
+      fieldsets: [
+        {
+          ...props.modifiedSchema?.fieldsets?.[0],
+          fields: (props.modifiedSchema?.fieldsets?.[0]?.fields || [])?.filter(
+            (f) => f !== 'safe_load_img',
+          ),
+        },
+        ...props?.modifiedSchema?.fieldsets?.slice(1),
+      ],
+    });
+  }
+
+  if (
+    isSvg &&
+    props?.modifiedSchema &&
+    !props?.modifiedSchema?.fieldsets?.[0]?.fields?.includes('safe_load_img')
+  ) {
+    props.setModifiedSchema({
+      ...props.modifiedSchema,
+      fieldsets: [
+        {
+          ...props.modifiedSchema.fieldsets?.[0],
+          fields: [
+            ...(props.modifiedSchema.fieldsets?.[0]?.fields || []),
+            'safe_load_img',
+          ],
+        },
+        ...props?.modifiedSchema?.fieldsets?.slice(1),
+      ],
+    });
+  }
   useEffect(() => {
     if (isSvg && data?.preview_image?.download) {
       fetch(data.preview_image.download)
